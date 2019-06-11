a2f313f70e9741fd566c2a64feb552f78a9b5f8a
T_T <tangyanqing95@sina.com>
2019-06-11 21:21:32 +0800

    todolist

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 import app from './modules/app'
+import todolist from './modules/todolist'
 import home from './modules/home'
 import login from './modules/login'
 
@@ -11,6 +12,7 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     app,
+    todolist,
     home,
     login
   },
